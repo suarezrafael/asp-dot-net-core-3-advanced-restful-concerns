33db527f3a2bdd4571d6263f65774ca117c6785c
Rafael Suarez <rafaelv_s@hotmail.com>
2021-08-13 10:14:16 -0300

    Adicionado todo projeto course library.

---
+++ CourseLibrary.API/ValidationAttributes/CourseTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -0,0 +1,43 @@
+﻿using CourseLibrary.API.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.ValidationAttributes
+{
+    public class CourseTitleMustBeDifferentFromDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, 
+            ValidationContext validationContext)
+        {
+            if (validationContext.ObjectInstance is ICollection<CourseForCreationDto>)
+            {
+                foreach (var courseCreation in (validationContext.ObjectInstance as ICollection<CourseForCreationDto>))
+                {
+                    var course = (CourseForManipulationDto)courseCreation;
+
+                    if (course.Title == course.Description)
+                    {
+                        return new ValidationResult(ErrorMessage,
+                            new[] { nameof(CourseForManipulationDto) });
+                    }
+                }
+            }
+            else
+            {
+                var course = (CourseForManipulationDto)validationContext.ObjectInstance;
+
+                if (course.Title == course.Description)
+                {
+                    return new ValidationResult(ErrorMessage,
+                        new[] { nameof(CourseForManipulationDto) });
+                }
+
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
