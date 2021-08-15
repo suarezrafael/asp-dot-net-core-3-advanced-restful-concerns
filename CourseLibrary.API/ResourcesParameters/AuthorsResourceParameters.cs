32b22dd934f52690853a77559b24ba4de6f3478f
Rafael Suarez <rafaelv_s@hotmail.com>
2021-08-15 10:46:06 -0300

    nova propriedade string OrderBy

--- CourseLibrary.API/ResourcesParameters/AuthorsResourceParameters.cs
+++ CourseLibrary.API/ResourcesParameters/AuthorsResourceParameters.cs
@@ -16,5 +16,6 @@ namespace CourseLibrary.API.ResourceParameters
             get => _pageSize;
             set => _pageSize = (value > maxPageSize) ? maxPageSize : value;         
         }
+        public string OrderBy { get; set; } = "Name";
     }
 }
