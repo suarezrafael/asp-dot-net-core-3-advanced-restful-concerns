675de05354aed2f091b33c09bd3c5221b65fa6b3
Rafael Suarez <rafaelv_s@hotmail.com>
2021-08-15 13:47:42 -0300

    classe propertyMapping

---
+++ CourseLibrary.API/ResourcesParameters/PropertyMapping.cs
@@ -0,0 +1,19 @@
+﻿using CourseLibrary.API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.ResourcesParameters
+{
+    public class PropertyMapping<TSource, TDestination> : IPropertyMapping
+    {
+        public Dictionary<string, PropertyMappingValue> _mappingDictionary { get; private set; }
+
+        public PropertyMapping(Dictionary<string, PropertyMappingValue> mappingDictionary)
+        {
+            _mappingDictionary = mappingDictionary ??
+                throw new ArgumentNullException(nameof(mappingDictionary));
+        }
+    }
+}
